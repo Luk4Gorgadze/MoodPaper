0edb607cb1674e25a5f60e9786656d1ab6bf94e2
Luka Gorgadze <97985323+LukeGorgadze@users.noreply.github.com>
2024-11-02 21:34:39 +0400

    up

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -201,9 +201,9 @@ CORS_ALLOW_HEADERS = [
 ]
 
 
-SESSION_COOKIE_HTTPONLY = True  # Changed to True for better security
-SESSION_COOKIE_SECURE = True
-CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_HTTPONLY = False  # Changed to True for better security
+SESSION_COOKIE_SECURE = False
+CSRF_COOKIE_SECURE = False
 CSRF_COOKIE_HTTPONLY = False
 
 # Frontend and cookie settings
