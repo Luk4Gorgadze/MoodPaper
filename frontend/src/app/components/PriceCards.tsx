4bd0b04e866bcb91a073dbfcb951d5ccc0a967cf
Luka Gorgadze <97985323+LukeGorgadze@users.noreply.github.com>
2024-11-05 21:41:46 +0400

    Updated prices

--- frontend/src/app/components/PriceCards.tsx
+++ frontend/src/app/components/PriceCards.tsx
@@ -57,16 +57,16 @@ export default function PriceCards() {
                 <div className="flex flex-col p-6 mx-auto w-full text-center text-gray-900 bg-white rounded-lg border border-gray-100 shadow dark:border-gray-600 xl:p-8 dark:bg-background dark:text-white">
                     <div className="flex flex-col h-full">
                         <h3 className="mb-4 text-2xl font-semibold">Standard</h3>
-                        <p className="font-light text-gray-500 sm:text-lg dark:text-gray-400">30 tokens and access to public library which stores your images</p>
+                        <p className="font-light text-gray-500 sm:text-lg dark:text-gray-400">20 tokens and access to public library which stores your images</p>
                         <div className="mt-auto">
                             <div className="flex justify-center items-baseline my-8">
-                                <span className="mr-2 text-5xl font-extrabold">$10</span>
+                                <span className="mr-2 text-5xl font-extrabold">$5</span>
                                 <span className="text-gray-500 dark:text-gray-400"></span>
                             </div>
                             <ul role="list" className="mb-8 space-y-4 text-left">
                                 <li className="flex items-center space-x-3">
                                     <svg className="flex-shrink-0 w-5 h-5 text-green-500 dark:text-green-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clipRule="evenodd"></path></svg>
-                                    <span>30 tokens</span>
+                                    <span>20 tokens</span>
                                 </li>
                                 <li className="flex items-center space-x-3">
                                     <svg className="flex-shrink-0 w-5 h-5 text-green-500 dark:text-green-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clipRule="evenodd"></path></svg>
@@ -79,7 +79,7 @@ export default function PriceCards() {
                             </ul>
                             {/* <button className="w-full px-4 py-2 bg-accent text-white font-bold rounded hover:bg-accent/80">Get Started</button> */}
                             {user && (
-                                <PaypalWrapper user={user} order_price={10} subscription_type="STANDARD" onPaymentSuccess={handlePaymentSuccess} />
+                                <PaypalWrapper user={user} order_price={5} subscription_type="STANDARD" onPaymentSuccess={handlePaymentSuccess} />
                             )}
                             {!user && (
                                 <div className="w-full px-4 py-2 bg-red-600 text-white font-bold rounded">Log In to Purchase</div>
@@ -93,16 +93,16 @@ export default function PriceCards() {
                 <div className="flex flex-col p-6 mx-auto w-full text-center text-gray-900 bg-white rounded-lg border border-gray-100 shadow dark:border-gray-600 xl:p-8 dark:bg-background dark:text-white">
                     <div className="flex flex-col h-full">
                         <h3 className="mb-4 text-2xl font-semibold">Premium</h3>
-                        <p className="font-light text-gray-500 sm:text-lg dark:text-gray-400">90 tokens and access to public library which stores your images</p>
+                        <p className="font-light text-gray-500 sm:text-lg dark:text-gray-400">60 tokens and access to public library which stores your images</p>
                         <div className="mt-auto">
                             <div className="flex justify-center items-baseline my-8">
-                                <span className="mr-2 text-5xl font-extrabold">$20</span>
+                                <span className="mr-2 text-5xl font-extrabold">$10</span>
                                 <span className="text-gray-500 dark:text-gray-400"></span>
                             </div>
                             <ul role="list" className="mb-8 space-y-4 text-left">
                                 <li className="flex items-center space-x-3">
                                     <svg className="flex-shrink-0 w-5 h-5 text-green-500 dark:text-green-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clipRule="evenodd"></path></svg>
-                                    <span>90 tokens</span>
+                                    <span>60 tokens</span>
                                 </li>
                                 <li className="flex items-center space-x-3">
                                     <svg className="flex-shrink-0 w-5 h-5 text-green-500 dark:text-green-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clipRule="evenodd"></path></svg>
@@ -115,7 +115,7 @@ export default function PriceCards() {
                             </ul>
                             {/* <button className="w-full px-4 py-2 bg-accent text-white font-bold rounded hover:bg-accent/80">Get Started</button> */}
                             {user && (
-                                <PaypalWrapper user={user} order_price={20} subscription_type="PREMIUM" onPaymentSuccess={handlePaymentSuccess} />
+                                <PaypalWrapper user={user} order_price={10} subscription_type="PREMIUM" onPaymentSuccess={handlePaymentSuccess} />
                             )}
                             {!user && (
                                 <div className="w-full px-4 py-2 bg-red-600 text-white font-bold rounded">Log In to Purchase</div>
